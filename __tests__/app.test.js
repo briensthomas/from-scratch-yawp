6b62d99a156de9ef5debae2e2cae47e6fa597568
Brien Thomas <thomasbrien94@gmail.com>
2022-08-15 15:19:54 -0700

    frame for GET route written, middleware for authenticated and authorized created

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,15 +2,12 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
-const UserService = require('../lib/services/UserService');
 
 
 const newUser = {
   email: 'test@example.com',
   password: '123456'
 };
-
-
 
 describe('backend-express-template routes', () => {
   beforeEach(() => {
