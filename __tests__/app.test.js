6dbc28e6b55ce65f1bec42e57653a108decd4cf3
Brien Thomas <thomasbrien94@gmail.com>
2022-08-15 12:55:00 -0700

    test for POST new user route works, also logs in a new user on creation.

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,28 +3,32 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
+const newUser = {
+  email: 'test@example.com',
+  password: '123456'
+};
+
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-});
-
-afterAll(() => {
-  pool.end();
-});
-
-it('#POST /api/v1/users, creates a new user', async () => {
-  const newUser = {
-    email: 'test@example.com',
-    password: '123456'
-  };
-  const res = await request(app).post('/api/v1/users').send(newUser);
-
-  expect(res.status).toBe(200);
-  expect(res.body).toEqual({
-    id: expect.any(String),
-    ...newUser
+  
+  afterAll(() => {
+    pool.end();
   });
-})
 
-;
+  it('#POST /api/v1/users, creates a new user', async () => {
+
+    const res = await request(app).post('/api/v1/users').send(newUser);
+    const { email } = newUser;
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual({ 
+      user: {
+        id: expect.any(String),
+        email 
+      },
+      message: 'You\'ve signed in!' });
+
+  });
+
+});
