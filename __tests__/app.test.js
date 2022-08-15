b06d5f0ed9b90c3736570ba78766ce8797f10f64
Brien Thomas <thomasbrien94@gmail.com>
2022-08-15 11:00:19 -0700

    init commit: created first test for POST route to create a new user

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,16 +1,30 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
+const request = require('supertest');
+const app = require('../lib/app');
 
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
-  });
+
   afterAll(() => {
     pool.end();
   });
 });
+
+it('#POST /api/v1/users, creates a new user', async () => {
+  const newUser = {
+    email: 'test@example.com',
+    password: '123456'
+  };
+  const res = await request(app).post('/api/v1/users').send(newUser);
+
+  expect(res.status).toBe(200);
+  expect(res.body).toEqual({
+    id: expect.any(String),
+    ...newUser
+  });
+})
+
+;
