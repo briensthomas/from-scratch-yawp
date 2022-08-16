5b454ffd9165bf362537c7282fbfe23b9a2f37d5
Brien Thomas <thomasbrien94@gmail.com>
2022-08-16 15:44:45 -0700

    the delete route passes the test for some reason??? It should not pass the test, this makes no sense at all

--- __tests__/review.test.js
+++ __tests__/review.test.js
@@ -3,7 +3,6 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
-const agent = request.agent(app);
 
 describe('backend-express-template routes', () => {
   beforeEach(() => {
@@ -15,21 +14,35 @@ describe('backend-express-template routes', () => {
   });
 
   it('#DELETE should return status 401 for unauthorized users', async () => {
-    const res = await request(app).delete('/api/reviews/1');
+    const res = await request(app).delete('/api/v1/reviews/1');
 
     expect(res.status).toBe(401);
   });
 
+  it('#DELETE should return status 200 for user with the same user_id', async () => {
+    const agent = request.agent(app);
+
+    await agent.post('/api/v1/users/sessions').send({
+      email: 'test@example.com',
+      password: '123456'
+    });
+
+    const res = await agent.delete('/api/v1/reviews/1');
+    console.log(res.body);
+    expect(res.status).toBe(200);
+  });
+
   it('#DELETE should return status 200 for admin', async () => {
+    const agent = request.agent(app);
+
     await agent.post('/api/v1/users/').send({
       email: 'admin',
       password: '123456'
     });
 
-    const res = await agent.delete('/api/reviews/1');
-
+    const res = await agent.delete('/api/v1/reviews/1');
+    console.log(res.body);
     expect(res.status).toBe(200);
   });
 
-  
 });
