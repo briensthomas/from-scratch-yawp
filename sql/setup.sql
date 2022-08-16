0e0391243a04d15ab5b18afe6c905796f20e63fe
Brien Thomas <thomasbrien94@gmail.com>
2022-08-15 17:49:47 -0700

    get restaurant by id, with nested reviews test passes for some reason

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,7 +1,8 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
-DROP TABLE IF EXISTS yawp_users;
-DROP TABLE IF EXISTS yawp_restaurants;
+DROP TABLE IF EXISTS yawp_users CASCADE;
+DROP TABLE IF EXISTS yawp_restaurants CASCADE;
+DROP TABLE IF EXISTS yawp_reviews;
 
 CREATE TABLE yawp_users (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -28,4 +29,21 @@ INSERT INTO yawp_restaurants (
     type
 )
 VALUES
-('El Burrito Azteca', 'Mexican')
+('El Burrito Azteca', 'Mexican');
+
+CREATE TABLE yawp_reviews (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    restaurant_id BIGINT,
+    user_id BIGINT,
+    content VARCHAR(255) NOT NULL,
+    FOREIGN KEY (restaurant_id) REFERENCES yawp_restaurants(id),
+    FOREIGN KEY (user_id) REFERENCES yawp_users(id)
+);
+
+INSERT INTO yawp_reviews (
+    restaurant_id,
+    user_id,
+    content
+)
+VALUES 
+(1, 1, 'Try the Burrito Azteca! Wet, with shredded beef, and ask for extra salsa verde!');
