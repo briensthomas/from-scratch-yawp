c8e908f542396294a9521780c53f0b575b5858e8
Brien Thomas <thomasbrien94@gmail.com>
2022-08-16 17:57:05 -0700

    /api/v1/restaurants?type= tests pass, not sure why it works but ok

--- __tests__/restaurant.test.js
+++ __tests__/restaurant.test.js
@@ -23,7 +23,7 @@ describe('backend-express-template routes', () => {
     });
   });
 
-  it.only('#GET /api/v1/restaurants?type= returns status 200', async () => {
+  it('#GET /api/v1/restaurants?type= returns status 200', async () => {
     const res = await request(app).get('/api/v1/restaurants?type=Mexican');
 
     expect(res.status).toBe(200);
@@ -32,10 +32,11 @@ describe('backend-express-template routes', () => {
   it('#GET /api/v1/restaurants?type= returns specific types of restaurants as a list', async () => {
     const res = await request(app).get('/api/v1/restaurants?type=Mexican');
 
-    expect(res.body[0]).toEqual({
+    expect(res.body).toEqual([{
+      id: expect.any(String),
       name: 'El Burrito Azteca',
       type: 'Mexican',
-    });
+    }]);
   });
 
   it('#GET restaurants/:restId, one restaurant with nested reviews', async () => {
